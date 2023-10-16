0cca643ff7b65c8128d4eece88728141c37f538b
Jaden <jadendonald03@gmail.com>
2023-10-16 04:56:18 -0500

    - Rendering has been mega optimized (You can tell I actually understand OpenGL now, when I made FirewoodEngine I was still on the fence) - Really close to having the Actor/Component based architecture done

--- IceCrystalEngine/Include/Ice/Core/Actor.h
+++ IceCrystalEngine/Include/Ice/Core/Actor.h
@@ -4,7 +4,10 @@
 
 #define ACTOR_H
 
+#include <vector>
+
 class Transform; // forward declaration because actor and transform depend on eachother
+class Component;
 
 class Actor
 {
@@ -12,12 +15,29 @@ class Actor
 public:
 	
 	Transform* transform;
+	std::vector<Component*>* components;
 
 	Actor();
 	~Actor();
 
+	// Add Component by Type
+	template <typename T>
+	T* AddComponent();
+	
+	// Get Component by Type
+	template <typename T>
+	T* GetComponent();
+
+	
+	// Remove Component by Pointer
+	void RemoveComponent(Component* component);
+	
+	// Delete Component by Pointer
+	void DeleteComponent(Component* component);
+
+	// Add Component by Pointer
+	void AddComponent(Component* component);
+
 };
-
-
 
 #endif
