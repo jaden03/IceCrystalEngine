7b4bc8382ba8c17efa5b9d38ff07c441627b961c
Jaden <jadendonald03@gmail.com>
2023-10-19 18:05:26 -0500

    + Questionable Shadows

--- IceCrystalEngine/Classes/Components/Light.cpp
+++ IceCrystalEngine/Classes/Components/Light.cpp
@@ -4,7 +4,7 @@
 
 DirectionalLight::DirectionalLight() : Component() 
 {
-	LightingManager::GetInstance().AddDirectionalLight(this);
+	Initialize();
 }
 
 DirectionalLight::DirectionalLight(glm::vec3 color, float strength) : Component()
@@ -12,13 +12,42 @@ DirectionalLight::DirectionalLight(glm::vec3 color, float strength) : Component(
 	this->color = color;
 	this->strength = strength;
 
-	LightingManager::GetInstance().AddDirectionalLight(this);
+	Initialize();
 }
 
 DirectionalLight::~DirectionalLight()
 {
 	LightingManager::GetInstance().RemoveDirectionalLight(this);
 }
+
+void DirectionalLight::Initialize()
+{
+	glGenTextures(1, &depthMap);
+	glBindTexture(GL_TEXTURE_2D, depthMap);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT, shadowMapResolution, shadowMapResolution, 0, GL_DEPTH_COMPONENT, GL_FLOAT, NULL);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
+	float borderColor[] = { 1.0f, 1.0f, 1.0f, 1.0f };
+	glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, borderColor);
+
+	LightingManager::GetInstance().AddDirectionalLight(this);
+}
+
+glm::mat4 DirectionalLight::GetLightSpaceMatrix()
+{
+	float nearPlane = 1.0f, farPlane = 100.0f;
+	glm::mat4 lightProjection = glm::ortho(-10.0f, 10.0f, -10.0f, 10.0f, nearPlane, farPlane);
+
+	glm::mat4 lightView = glm::lookAt(transform->position, transform->position + transform->forward, glm::vec3(0.0f, 1.0f, 0.0f));
+	glm::mat4 lightSpaceMatrix = lightProjection * lightView;
+
+	return lightSpaceMatrix;
+}
+
+
+
 
 
 PointLight::PointLight() : Component() 
--- IceCrystalEngine/Classes/Core/SceneInitializer.cpp
+++ IceCrystalEngine/Classes/Core/SceneInitializer.cpp
@@ -27,28 +27,46 @@ void SceneInitializer::InitializeScene()
     testActor->transform->Translate(0, -4, 5);
 
 
-	Actor* sun = new Actor("Sun");
-    Renderer* sunRenderer = new Renderer(FileUtil::AssetDir + "Models/cube.obj");
+	Actor* sun = new Actor("Sun", "sun");
+    Renderer* sunRenderer = new Renderer(FileUtil::AssetDir + "Models/cone.obj");
 	sun->AddComponent(sunRenderer);
     sun->AddComponent<DirectionalLight>();
 
-    sun->transform->Rotate(-45, 45, 0);
+	// set the rotation quat to be at an angle and angled down
+	sun->transform->Rotate(230, 0, 0);
+	sun->transform->Translate(0, 0, 0);
 	sun->transform->scale = glm::vec3(0.2f, 0.2f, 0.2f);
 
 
-	Actor* pointLight = new Actor("Point Light", "PointLight1");
+	
+	//Actor* sun2 = new Actor("Sun2");
+	//Renderer* sunRenderer2 = new Renderer(FileUtil::AssetDir + "Models/cube.obj");
+	//sun2->AddComponent(sunRenderer2);
+	//sun2->AddComponent<DirectionalLight>();
+
+	//// set the rotation quat to be at an angle and angled down (different from the first sun)
+	//sun2->transform->Rotate(230, -45, 0);
+	//sun2->transform->Translate(2, 0, 0);
+	//sun2->transform->scale = glm::vec3(0.2f, 0.2f, 0.2f);
+
+
+
+
+	/*Actor* pointLight = new Actor("Point Light", "PointLight1");
 	PointLight* pointLightComponent = pointLight->AddComponent<PointLight>();
 	pointLightComponent->color = glm::vec3(1.0f, 0.0f, 0.0f);
+	pointLight->transform->Translate(0, -3, 5);
 
 	Actor* pointLight2 = new Actor("Point Light", "PointLight2");
 	PointLight* pointLightComponent2 = pointLight2->AddComponent<PointLight>();
-	pointLightComponent2->color = glm::vec3(0.0f, 1.0f, 0.0f);
+	pointLightComponent2->color = glm::vec3(0.0f, 0.0f, 1.0f);
+	pointLight2->transform->Translate(0, -3, 4);*/
 
-	Actor* pointLight3 = new Actor("Point Light", "PointLight3");
+	/*Actor* pointLight3 = new Actor("Point Light", "PointLight3");
 	PointLight* pointLightComponent3 = pointLight3->AddComponent<PointLight>();
-	pointLightComponent3->color = glm::vec3(0.0f, 0.0f, 1.0f);
+	pointLightComponent3->color = glm::vec3(0.0f, 0.0f, 1.0f);*/
 
-    // This takes a few seconds to load because spock49.obj is not an optimized model at all
+     //This takes a few seconds to load because spock49.obj is not an optimized model at all
 	/*Actor* testActor2 = new Actor("Test Actor 2", "Test");
 	Material* material2 = new Material(FileUtil::AssetDir + "Materials/spock.mat");
 	Renderer* renderer2 = new Renderer(FileUtil::AssetDir + "Models/spock49.obj", material2);
