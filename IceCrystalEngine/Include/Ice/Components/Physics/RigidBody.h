ae59e5167440137c8b19387f262513305485e9ea
Jaden <jadendonald03@gmail.com>
2025-12-06 03:13:21 -0600

    + Initial implmentation of Jolt Physics

---
+++ IceCrystalEngine/Include/Ice/Components/Physics/RigidBody.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+
+#include <Ice/Core/Component.h>
+#include <Ice/Components/Physics/Collider.h>
+
+#include <Jolt/Jolt.h>
+#include <Jolt/Physics/Collision/Shape/BoxShape.h>
+#include <Jolt/Physics/Body/BodyCreationSettings.h>
+
+
+#include <glm/glm.hpp>
+
+#include <Ice/Core/PhysicsManager.h>
+
+class RigidBody : public Component
+{
+public:
+    RigidBody(float mass = 1.0f);
+    virtual ~RigidBody();
+
+    void Ready() override;    // Called when actor/component is initialized
+    void Update() override;   // Called every frame
+
+    float mass;
+    
+    Collider* collider;
+    JPH::Body* body = nullptr;
+
+private:
+
+};
