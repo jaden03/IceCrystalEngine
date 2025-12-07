2f2155c2843571e6fe4b06c2d792df5746d7ac1d
Jaden <jadendonald03@gmail.com>
2025-12-07 16:37:34 -0600

    +

---
+++ LanderGame/Core/Game.cpp
@@ -0,0 +1,27 @@
+﻿#include "Game.h"
+
+#include <iostream>
+#include <ostream>
+
+void Game::OnInit()
+{
+    std::cout << "Hello World!" << std::endl;
+}
+
+void Game::OnUpdate(float deltaTime)
+{
+    
+}
+
+void Game::OnFixedUpdate(float fixedDelaTime)
+{
+    
+}
+
+
+void Game::OnShutdown()
+{
+    std:: cout << "Bye!" << std::endl;
+}
+
+
---
+++ LanderGame/Core/Game.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+
+#include <Ice/Core/IGame.h>
+
+class Game : public IGame
+{
+public:
+    void OnInit() override;
+    void OnUpdate(float deltaTime) override;
+    void OnFixedUpdate(float fixedDelaTime) override;
+    void OnShutdown() override;
+private:
+    
+};
